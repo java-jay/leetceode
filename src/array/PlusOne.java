460f3016de639a10a4e51ba89a91bc4fbe925a18
JAY <947110684@qq.com>
2019-11-21 00:00:37 +0800

    11.20

---
+++ src/array/PlusOne.java
@@ -0,0 +1,29 @@
+package array;
+
+/**
+ * 加一
+ * 末位无进位，则末位加一即可，因为末位无进位，前面也不可能产生进位，比如45 => 46
+ * 末位有进位，在中间位置进位停止，则需要找到进位的典型标志，即为当前位%10后为0，则前一位加1，直到不为0为止，比如499 => 500
+ * 末位有进位，并且一直进位到最前方导致结果多出一位，对于这种情况，需要在第2种情况遍历结束的基础上，进行单独处理，比如999 => 1000
+ */
+public class PlusOne {
+    public static void main(String[] args) {
+        PlusOne plusOne = new PlusOne();
+        plusOne.plusOne(new int[]{9, 9, 9});
+    }
+
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] %= 10;
+            //如果是正常相加就直接返回，如果有进位就会进行下一次循环
+            if (digits[i] != 0)
+                return digits;
+        }
+        //循环结束仍未返回，说明是999类型，需要定义新数组
+        digits = new int[len + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
--- src/string/AddBinary.java
+++ src/string/AddBinary.java
@@ -1,28 +1,33 @@
 package string;
 
 /**
+ * 67. 二进制求和
  * 整体思路是将两个字符串较短的用0补齐，使得两个字符串长度一致，然后从末尾进行遍历计算，得到最终结果
- * 本题解中大致思路与上述一致，但由于字符串操作原因，不确定最后的结果是否会多出一位进位，所以会有2种处理方式
  *
- * 第一种，在进行计算时直接拼接字符串，会得到一个反向字符，需要最后再进行翻转
- * 第二种，按照位置给结果字符赋值，最后如果有进位，则在前方进行字符串拼接添加进位
+ * 从右往左相加，把每次相加后的值加入字符串中，最后如果还有进位，就将它加到末尾，然后反转字符串
  */
 public class AddBinary {
     public static void main(String[] args) {
         AddBinary addBinary = new AddBinary();
-        addBinary.addBinary("11","1");
+        addBinary.addBinary("11", "1");
     }
+
     public String addBinary(String a, String b) {
         StringBuilder ans = new StringBuilder();
         int ca = 0;
-        for(int i = a.length() - 1, j = b.length() - 1;i >= 0 || j >= 0; i--, j--) {
+        //从后往前遍历
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
+            //让当前值为进位
             int sum = ca;
-            sum += i >= 0 ? a.charAt(i) - '0' : 0;
-            sum += j >= 0 ? b.charAt(j) - '0' : 0;
-            ans.append(sum % 2);
-            ca = sum / 2;
+            //因为字符长度可能不一样，当索引为负数时，当做加了个0
+            sum += (i >= 0 ? a.charAt(i) - '0' : 0);
+            sum += (j >= 0 ? b.charAt(j) - '0' : 0);
+            ans.append(sum % 2);//将计算后的当前位加入字符串
+            ca = sum / 2;//进位
         }
+        //如果最后还有进位，就将它加入字符串中
         ans.append(ca == 1 ? ca : "");
+        //由于是从右往左遍历，所以最后要逆转
         return ans.reverse().toString();
     }
 }
