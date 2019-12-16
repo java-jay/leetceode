aabeaa0f53fa95a9dff1b71163f4fcbaa6bdd5b3
JAY <947110684@qq.com>
2019-12-16 23:54:29 +0800

    12.16

---
+++ src/binarySearch/Mysort.java
@@ -0,0 +1,35 @@
+package binarySearch;
+
+public class Mysort {
+    public static void main(String[] args) {
+        new Mysort().mySqrt(4);
+    }
+
+    public int mySqrt(int x) {
+        if (x <= 1) {
+            return x;
+        }
+        int l = 1, h = x;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            int sqrt = x / mid;
+            if (sqrt == mid) {
+                return mid;
+            } else if (mid > sqrt) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return h;
+    }
+
+    //牛顿法
+    public int mySqrt2(int a) {
+        long x = a;
+        while (x * x > a) {
+            x = (x + a / x) / 2;
+        }
+        return (int) x;
+    }
+}
