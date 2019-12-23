4067cf74582821eae3256ac88f2ec7a2971fa94c
JAY <947110684@qq.com>
2019-12-23 23:38:50 +0800

    12.23

---
+++ src/algorithm/math/ToHex.java
@@ -0,0 +1,17 @@
+package algorithm.math;
+
+/**
+ * 405. 数字转换为十六进制数
+ */
+public class ToHex {
+    public String toHex(int num) {
+        if (num == 0) return "0";
+        char[] map = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            sb.append(map[num & 0b1111]);//取后4位
+            num >>>= 4;//无符号右移4位,每4位数转为16进制的1位
+        }
+        return sb.reverse().toString();
+    }
+}
